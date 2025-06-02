2561af18dc2cfb569b5e9f95c03f0c0ab3178e92
The3xds <88211036+The3xds@users.noreply.github.com>
2025-06-02 20:07:17 +0100

    v1.0.0

---
+++ bulk-installer/nonWingetApps.js
@@ -0,0 +1,4 @@
+// Non-Winget Apps Data
+const nonWingetApps = [
+    { "name": "Vencord", "icon": "https://vencord.dev/assets/logo-nav-oneko-padding.png", "downloadUrl": "https://github.com/Vencord/Installer/releases/latest/download/VencordInstaller.exe", "category": "Utility" }
+];
